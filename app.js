4858e78427c368c2d70d44684a10d4afbbc5837d
Amankr705 <ak.1000aman@gmail.com>
2022-12-18 00:41:22 +0530

    Final

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+
+var btnTranslate = document.querySelector("#btn-translate");
+
 var outputDiv = document.querySelector("#output");
 
 // var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
